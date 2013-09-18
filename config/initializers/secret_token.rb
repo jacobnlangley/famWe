8b238f3590bed70b13bb250f1dc1a7be0687626b
jacobnlangley <jacob@jacobnlangley.com>
2013-09-17 23:25:27 -0400

    Created famWe application and added statuses.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FamWe::Application.config.secret_key_base = 'de9929c87c55178550f83876023698f689165accae366cb33ac811bd65f3be18848b3d564c024e3d5bb261457abfd4255009671ce1a61610e1b6324b689aeb1e'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+FamWe::Application.config.session_store :cookie_store, key: '_famWe_session'
